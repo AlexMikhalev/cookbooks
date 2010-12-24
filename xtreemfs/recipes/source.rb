79e45ee0c286a1441f23a9e4b52329ae1086eefe
AlexMikhalev <alex.mikhalev@gmail.com>
2010-12-24 15:56:04 +0000

    Added generate_uuid scripts for all config files

--- xtreemfs/recipes/source.rb
+++ xtreemfs/recipes/source.rb
@@ -1,24 +1,36 @@
 include_recipe "build-essential"
+include_recipe "subversion"
 include_recipe "java"
-%w{wget curl uuid-runtime default-jre-headless fuse-utils attr openjdk-6-jdk ant libfuse-dev libglobus-openssl-dev}.each do |devpkg|
+%w{wget curl uuid-runtime default-jre-headless fuse-utils attr subversion ant openjdk-6-jdk  libssl-dev libfuse-dev fuse-utils}.each do |devpkg|
   package devpkg
 end
 
 xtreemfs_version = node[:xtreemfs][:version]
-configure_flags = node[:xtreemfs][:configure_flags].join(" ")
+# configure_flags = node[:xtreemfs][:configure_flags].join(" ")
+# 
+# remote_file "/tmp/XtreemFS-#{xtreemfs_version}.tar.gz" do
+#   source "http://xtreemfs.googlecode.com/files/XtreemFS-#{xtreemfs_version}.tar.gz"
+#   action :create_if_missing
+# end 
 
-remote_file "/tmp/XtreemFS-#{xtreemfs_version}.tar.gz" do
-  source "http://xtreemfs.googlecode.com/files/XtreemFS-#{xtreemfs_version}.tar.gz"
-  action :create_if_missing
-end
+# subversion "CouchDB Edge" do
+#   repository "http://svn.apache.org/repos/asf/couchdb/trunk"
+#   revision "HEAD"
+#   destination "/opt/mysources/couch"
+#   action :sync
+# end 
+
 
 bash "compile_xtreemfs_source" do
   cwd "/tmp"
   code <<-EOH
-    tar zxf XtreemFS-#{xtreemfs_version}.tar.gz
-    cd XtreemFS-#{xtreemfs_version} && ./configure #{configure_flags}
-    make server && make install
+    svn checkout http://xtreemfs.googlecode.com/svn/branches/XtreemFS-1.2.4
+    cd XtreemFS-1.2.4
+    make && make install
     /etc/xos/xtreemfs/postinstall_setup.sh
+    /etc/xos/xtreemfs/generate_uuid /etc/xos/xtreemfs/dirconfig.properties
+    /etc/xos/xtreemfs/generate_uuid /etc/xos/xtreemfs/mrcconfig.properties
+    /etc/xos/xtreemfs/generate_uuid /etc/xos/xtreemfs/osdconfig.properties
   EOH
 end
 
